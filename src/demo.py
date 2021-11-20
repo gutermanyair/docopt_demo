4759d3bf3a0c1fc6013dd8c97d929018d3d6bd02
Yair Guterman <yairguterman1@gmail.com>
2021-11-19 18:48:38 -0800

    Added main function

--- src/demo.py
+++ src/demo.py
@@ -14,6 +14,12 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt['<arg4>'])
-print(opt)
-print(type(opt))
+
+def main(arg1, arg2, arg3, arg4):
+    
+    print(opt['<arg4>'])
+    print(opt)
+    print(type(opt))
+    
+if __name__ == "__main__":
+    main(opt["<arg1>"], opt["--arg2"],opt["--arg3"],opt["<arg4>"])
